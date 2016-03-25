95521a71950145a5fbd602608bc0bce6273c4210
omarmrivas <omarmrivas@gmail.com>
2016-03-24 20:25:33 -0600

    With degree of parallelism

--- Program.fs
+++ Program.fs
@@ -4,6 +4,7 @@
 
 open Microsoft.FSharp.Math
 open Microsoft.FSharp.Collections
+open System.Threading.Tasks
 
 let initial_state best_so_far = 
     let vertices = [(0N, 0N)
@@ -13,7 +14,6 @@ let initial_state best_so_far =
                             Some (vertex, g) -> g
                           | None -> failwith "Impossible to raise")
               Graph.empty_graph vertices
-        |> Library.tap (fun g -> printfn "Initial state: %A" g)
 
 let goal n best_so_far (g : Graph.PlanarGraph) =
     if List.length g.vertices = n && g.crossing_number <= best_so_far n
@@ -21,6 +21,8 @@ let goal n best_so_far (g : Graph.PlanarGraph) =
     else false
 
 let succesor best_so_far (g : Graph.PlanarGraph) : (Graph.Vertex * Graph.PlanarGraph) list =
+    let options = ParallelOptions()
+    printfn "Configuring degree of parallelism: %A" options.MaxDegreeOfParallelism
     let center ((ux,uy),(vx,vy),(wx,wy)) = ((ux+vx+wx)/3N, (uy+vy+wy)/3N)
     let center' l = l |> List.fold (fun (cx,cy) (vx,vy) -> (cx+vx, cy+vy)) (0N,0N)
                       |> (fun (cx, cy) -> let n = l |> List.length
@@ -29,18 +31,18 @@ let succesor best_so_far (g : Graph.PlanarGraph) : (Graph.Vertex * Graph.PlanarG
     Graph.graph_to_gnuplot ("graph" + ((string << List.length) g.vertices)) g
     List.map (fun t -> (center t, Graph.T t)) g.triangles @ List.map (fun p -> (center' p, Graph.P p)) g.non_triangles
         |> Library.tap (fun arr -> printfn "Triangles & non-triangles: %A" (List.length arr))
-        |> List.toArray
-        |> Array.Parallel.choose (Graph.crossing_number best_so_far g)
-        |> Array.Parallel.choose (Graph.add_vertex best_so_far)
-        |> Library.tap (fun l -> printfn "Succesors with good crossing number: %A" (Array.length l))
-        |> Library.tap (fun l -> l |> Array.map (fun (_, g) -> g.crossing_number)
+        |> PSeq.withDegreeOfParallelism options.MaxDegreeOfParallelism
+        |> PSeq.choose (Graph.crossing_number best_so_far g)
+        |> PSeq.choose (Graph.add_vertex best_so_far)
+        |> Library.tap (fun l -> printfn "Succesors with good crossing number: %A" (PSeq.length l))
+        |> Library.tap (fun l -> l |> PSeq.map (fun (_, g) -> g.crossing_number)
                                    |> set
                                    |> printfn "Crossing numbers: %A")
         // Sort w.r.t crossing number and complexity of new vertex
-        |> Array.map (fun (v, g) -> (v,g,(String.length << string) v))
-        |> Array.sortBy (fun (_, g, i) -> (g.crossing_number, i))
-        |> Array.map (fun (v, g, _) -> (v, g))
-        |> Array.toList
+        |> PSeq.map (fun (v, g) -> (v,g,(String.length << string) v))
+        |> PSeq.sortBy (fun (_, g, i) -> (g.crossing_number, i))
+        |> PSeq.map (fun (v, g, _) -> (v, g))
+        |> PSeq.toList
 
 [<EntryPoint>]
 let main argv =
